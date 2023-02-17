a52c83732a31f95844851eb0d7566e0a7705fadb
jihch <haoqibaobaoji@126.com>
2023-02-17 09:43:11 +0800

    +1

---
+++ src/main/java/io/github/jihch/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package io.github.jihch.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.jihch.bean.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
